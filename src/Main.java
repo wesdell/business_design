42dd631e8376df94809e165cc36fd7f787fa4e5c
wesdell <wesdell17@gmail.com>
2024-05-22 14:51:14 -0500

    Generate design and project structure.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Dado cliente, empresa, productos
+    Business business = new Business();
+    Customer sebastian = new Customer("Sebastian");
+    business.addProduct(
+            new Product("Mochila", 89.99, 5),
+            new Product("Zapatos", 50.00, 2),
+            new Product("Linterna", 8.49, 10),
+            new Product("Gorra", "27,99", 4)
+    );
+
+    // Cuando cliente compra productos
+    business.showProducts();
+
+    // Entonces se emite una factura
   }
 }
