cb335da8c18d3fdb01a25bf0046ea41803fdff6f
Wesdell <wesdell17@gmail.com>
2024-05-22 22:01:24 -0500

    Create Product class with its methods to modify stock and to get price.

---
+++ src/Product.java
@@ -0,0 +1,30 @@
+public class Product {
+  private final String name;
+  private final double price;
+  private int stock;
+
+  public Product(String name, double price, int stock) {
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  public int reduceStock(int amountToReduce) {
+    if (amountToReduce > this.stock) {
+      amountToReduce = this.stock;
+      this.stock = 0;
+    } else {
+      this.stock -= amountToReduce;
+    }
+    return amountToReduce;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  @Override
+  public String toString() {
+    return this.name;
+  }
+}
