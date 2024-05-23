f3fad74e7d84c4ab9456521fde2cb7935b3dbc5d
Wesdell <wesdell17@gmail.com>
2024-05-22 20:52:59 -0500

    Fix code type errors.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
   public static void main(String[] args) {
     // Dado cliente, empresa, productos
-    Business business = new Business();
+    Business business = new Business("SG");
     Customer sebastian = new Customer("Sebastian");
     business.addProduct(
             new Product("Mochila", 89.99, 5),
             new Product("Zapatos", 50.00, 2),
             new Product("Linterna", 8.49, 10),
-            new Product("Gorra", "27,99", 4)
+            new Product("Gorra", 27.99, 4)
     );
 
     // Cuando cliente compra productos
     Receipt receipt = business.sellProducts(
             sebastian,
             new PurchasedProduct(business.getProductById(1), 2),
-            new PurchasedProduct(empresa.getProductById(2), 1),
-            new PurchasedProduct(empresa.getProductById(3), 2),
-            new PurchasedProduct(empresa.getProductById(4), 4)
-            );
+            new PurchasedProduct(business.getProductById(2), 1),
+            new PurchasedProduct(business.getProductById(3), 2),
+            new PurchasedProduct(business.getProductById(4), 4)
+    );
 
     // Entonces se emite una factura
     System.out.println(receipt);
